57a245bf225b656ae3e817140896ea4dc4ee5e6b
wladd <wladd@developer.com>
2024-08-15 23:48:15 +0500

    optimization ORM-requests (was duplicate sql-queries)

--- women/views.py
+++ women/views.py
@@ -12,32 +12,10 @@ menu = [
     {"title": "Войти", "url_name": "login"},
 ]
 
-data_db = [
-    {
-        "id": 1,
-        "title": "Анджелина Джоли",
-        "content": """(англ. Angelina Jolie, при рождении Войт (англ. Voight), ранее Джоли Питт (англ. Jolie Pitt); род. 4 июня 1975, Лос-Анджелес, Калифорния, США) — американская актриса кино, телевидения и озвучивания, кинорежиссёр, сценаристка, продюсер, фотомодель, посол доброй воли ООН.
-    Обладательница премии «Оскар», трёх премий «Золотой глобус» (первая актриса в истории, три года подряд выигравшая премию) и двух «Премий Гильдии киноактёров США».""",
-        "is_published": True,
-    },
-    {
-        "id": 2,
-        "title": "Марго Робби",
-        "content": "Биография Марго Робби",
-        "is_published": False,
-    },
-    {
-        "id": 3,
-        "title": "Джулия Робертс",
-        "content": "Биография Джулия Робертс",
-        "is_published": True,
-    },
-]
-
 
 def index(request):
 
-    posts = Women.published.all()
+    posts = Women.published.all().select_related('cat')
 
     data = {
         "title": "Главная страница",
@@ -52,7 +30,6 @@ def about(request):
     data = {
         "title": "О сайте",
         "menu": menu,
-        "posts": data_db,
     }
     return render(request, "women/about.html", data)
 
@@ -84,7 +61,7 @@ def login(request):
 
 def show_category(request, cat_slug):
     category = get_object_or_404(Category, slug=cat_slug)
-    posts = Women.published.filter(cat_id=category.pk)
+    posts = Women.published.filter(cat_id=category.pk).select_related('cat')
     data = {
         "title": f"Рубрика: {category.name}",
         "menu": menu,
@@ -96,7 +73,7 @@ def show_category(request, cat_slug):
 
 def show_tag_postlist(request, tag_slug):
     tag = get_object_or_404(TagPost, slug=tag_slug)
-    posts = tag.tags.filter(is_published=1)
+    posts = tag.tags.filter(is_published=1).select_related('cat')
 
     data = {
         "title": f"Тег: {tag.tag}",
