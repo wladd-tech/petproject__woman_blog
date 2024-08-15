1b6783b887200475d61a3fc6d3bd3a91be52d2e0
wladd <wladd@developer.com>
2024-08-15 23:31:56 +0500

    tags and husbands to woman model

--- women/models.py
+++ women/models.py
@@ -16,6 +16,7 @@ class Women(models.Model):
     is_published = models.BooleanField(default=True)
     cat = models.ForeignKey("Category", on_delete=models.PROTECT, related_name="posts")
     tags = models.ManyToManyField("TagPost", blank=True, related_name="tags")
+    husband = models.OneToOneField('Husband', on_delete=models.SET_NULL, null=True, blank=True, related_name='woman')
 
     published = PublishedManager()
     objects = models.Manager()
@@ -51,3 +52,11 @@ class TagPost(models.Model):
 
     def get_absolute_url(self):
         return reverse("tag", kwargs={"tag_slug": self.slug})
+
+
+class Husband(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
