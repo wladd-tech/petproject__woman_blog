6a12822c62183f48097aee8e10d4d5690047428e
wladd <wladd@developer.com>
2024-08-17 14:24:41 +0500

    custom validator (russian-chars) for post-forms

--- women/forms.py
+++ women/forms.py
@@ -1,10 +1,27 @@
 from django import forms
 from .models import Category, Husband
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
+
+
+
+@deconstructible
+class RussianValidator:
+    ALLOWED_CHARS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзиклмнопрстуфхцчшщьыъэюя0123456789-"
+    code = 'russian'
+    
+    def __init__(self, message=None):
+        self.message = message if message else "Должны присутствовать только русские символы, дефис или пробел"
+
+    def __call__(self, value, *args, **kwargs):
+        if not (set(value) <= set(self.ALLOWED_CHARS)):
+            raise ValidationError(self.message, code=self.code)
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea, required=False)
-    is_published = forms.BooleanField(required=False)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
-    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False)
+    title = forms.CharField(max_length=255, label='Заголовок', widget=forms.TextInput(attrs={'class': "form-input"}), validators=[RussianValidator(),])
+    slug = forms.SlugField(max_length=255, label='URL', validators=[ MinLengthValidator(5), MaxLengthValidator(100)])
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols':50, 'rows': 5}), required=False, label='Контент')
+    is_published = forms.BooleanField(required=False, label='Публикация', initial=True)
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категории', empty_label='Не выбрано')
+    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False, label='Муж', empty_label='Не выбрано')
--- women/views.py
+++ women/views.py
@@ -53,7 +53,11 @@ def addpage(request):
     if request.method == 'POST':
         form = AddPostForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Ошибка добавления статьи')
     else:
         form = AddPostForm()
 
