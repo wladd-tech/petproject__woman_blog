61f12d6570756357d2d1eb7f3ef58257d600e954
wladd <wladd@developer.com>
2024-08-18 04:41:13 +0500

    refactoring login-logout users

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,12 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import get_user_model
 from django import forms
 
 
-class LoginUserForm(forms.Form):
+class LoginUserForm(AuthenticationForm):
     username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'password']
