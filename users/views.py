45baf045d4b200a441a7095fec896055037afcbc
wladd <wladd@developer.com>
2024-08-18 20:51:42 +0500

    users password reset + users profile photo upload

--- users/views.py
+++ users/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.views import LoginView, PasswordChangeView
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, UpdateView
 
+from app_config import settings
+
 from .forms import (
     LoginUserForm,
     RegisterUserForm,
@@ -29,7 +31,10 @@ class ProfileUser(LoginRequiredMixin, UpdateView):
     model = get_user_model()
     form_class = ProfileUserForm
     template_name = "users/profile.html"
-    extra_context = {"title": "Профиль пользователя"}
+    extra_context = {
+        "title": "Профиль пользователя",
+        "default_image": settings.DEFAULT_USER_IMAGE,
+        }
 
     def get_success_url(self):
         return reverse_lazy("users:profile")
