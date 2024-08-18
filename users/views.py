9e484748c28e831499ae645564d83294a8499198
wladd <wladd@developer.com>
2024-08-18 15:27:08 +0500

    user password-change

--- users/views.py
+++ users/views.py
@@ -1,13 +1,15 @@
-from django.contrib.auth import authenticate, login, logout, get_user_model
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, PasswordChangeView
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, UpdateView
 
-from .forms import LoginUserForm, RegisterUserForm, ProfileUserForm
+from .forms import (
+    LoginUserForm,
+    RegisterUserForm,
+    ProfileUserForm,
+    UserPasswordChangeForm,
+)
 
 
 class LoginUser(LoginView):
@@ -34,3 +36,9 @@ class ProfileUser(LoginRequiredMixin, UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+
+class UserPasswordChange(PasswordChangeView):
+    form_class = UserPasswordChangeForm
+    success_url = reverse_lazy("users:password_change_done")
+    template_name = "users/password_change_form.html"
