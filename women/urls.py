023e3e26b07263cb58b38fb491ce25ac0e2dc396
wladd <wladd@developer.com>
2024-08-19 12:16:22 +0500

    remove old pages

--- women/urls.py
+++ women/urls.py
@@ -8,9 +8,7 @@ register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
     path("", views.WomenHome.as_view(), name="home"),
-    path("about/", views.about, name="about"),
     path("addpage/", views.AddPage.as_view(), name="add_page"),
-    path("contact/", views.contact, name="contact"),
     path("login/", views.login, name="login"),
     path('post/<slug:post_slug>/', views.ShowPost.as_view(), name="post"),
     path('category/<slug:cat_slug>/', views.WomenCategory.as_view(), name="category"),
